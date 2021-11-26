6f1c3e305027463ab8d41e86b0e7696497c011a8
dancejod <dancejova.daniela@gmail.com>
2021-11-26 21:39:22 +0100

    Evidentne som nepochopila zadanie, takze opravujem znova rocny cyklus lol

--- prutoky.py
+++ prutoky.py
@@ -39,22 +39,18 @@ with open ("vstup.csv", encoding="utf-8") as vstup, open("sample_vystup.csv","w"
 
     prvy_riadok_roku = 1
     aktualny_rok = 0
-    Prvy_den = 0
-    Prvy_mesiac = 0                           ### Konstanty, ktore sa vyuziju na spravne pocita
 
     for riadok in reader:
 
         if aktualny_rok == 0:
-            aktualny_rok = int(riadok[2])
-            Prvy_mesiac = int(riadok[3])
-            Prvy_den = int(riadok[4])           
+            aktualny_rok = int(riadok[2])       
         
         if prvy_riadok_roku == 1:
             prvy_riadok_roku = riadok               ### Ulozi prvy riadok vo vstupe
 
         zasobnik.append(float(riadok[5]))           ### Vola do zasobnika, kym sa nezastavi podla dalsej podmienky
 
-        if int(riadok[2]) != aktualny_rok and int(riadok[3]) == Prvy_mesiac and int(riadok[4]) == Prvy_den:         ### Vyhodnotenie zasobnika s ohladom na priestupne roky
+        if int(riadok[2]) != aktualny_rok:         ### Vyhodnotenie zasobnika s ohladom na priestupne roky
             priemer = (sum(zasobnik))/len(zasobnik)
             prvy_riadok_roku[5] = ("{0:.4f}".format(priemer))        ### Formatovanie na 4 desatinne miesta
             writer_365.writerow(prvy_riadok_roku)
