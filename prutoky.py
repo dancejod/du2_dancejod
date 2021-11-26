efd10c023b734c8193a2e88c6d1257a176d12d96
dancejod <dancejova.daniela@gmail.com>
2021-11-26 18:23:18 +0100

    Pridana podmienka pre vypriemerovanie vstupu nedelitelneho 7

--- prutoky.py
+++ prutoky.py
@@ -15,12 +15,17 @@ with open ("sample_vstup.csv", encoding="utf-8") as csvinfile, open("sample_vyst
         if cislo_riadku % 7 == 1:
             prvy_riadok = riadok            ### Prvy riadok z tyzdna sa ulozi do premennej, v ktorej sa uz len modifikuje priemerny prietok
 
-        zasobnik.append(float(riadok[5].strip()))
+        zasobnik.append(float(riadok[5]))
            
         if len(zasobnik) == 7:
-            priemer = (sum(zasobnik))/7
+            priemer = (sum(zasobnik))/len(zasobnik)
             prvy_riadok[5] = ("{0:.4f}".format(priemer))        ### Zaistuje vypisanie priemerov na 4 desatinne miesta
             writer.writerow(prvy_riadok)
             zasobnik.clear()                    ### Vyprazdnenie zasobnika po vypocitani priemeru z tyzdna
-        #else:
-            
+
+    if len(zasobnik) != 0:                      ### Pokial vstup nie je delitelny 7, vypriemeruje sa zvysok zasobnika a prida sa k prvemu dnu nasledujuceho tyzdna
+        print(zasobnik)
+        priemer = (sum(zasobnik))/len(zasobnik) 
+        prvy_riadok[5] = ("{0:.4f}".format(priemer))       
+        writer.writerow(prvy_riadok)
+        zasobnik.clear()
